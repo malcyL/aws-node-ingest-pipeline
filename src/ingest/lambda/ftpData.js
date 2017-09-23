19413cb09dac5d690372d03e4cc37455d37dac84
MalcyL <malcolm@landonsonline.me.uk>
2017-09-23 14:05:21 +0100

    Connect to FTP server and list files.

--- src/ingest/lambda/ftpData.js
+++ src/ingest/lambda/ftpData.js
@@ -1,7 +1,32 @@
 'use strict';
 
+var JSFtp = require('jsftp');
+
 module.exports.ftpData = (event, context) => {
   const time = new Date();
   console.log(`Your cron function "${context.functionName}" ran at ${time}`);
+
+  var host = process.env.FTP_HOST;
+  var username = process.env.FTP_USERNAME;
+  var password = process.env.FTP_PASSWORD;
+  console.log(`Connecting to ftp host "${process.env.FTP_HOST}"`);
+
+  var Ftp = new JSFtp({
+    host: host,
+    user: username,
+    pass: password
+  });
+
+  Ftp.ls("*", function(err, data) {
+    if (err) {
+      console.error(err);
+      context.fail(err);
+    } else {
+      data.forEach(function (file) {
+        console.log(file.name); 
+      });
+      context.succeed('OK');
+    }
+  });
 };
 
