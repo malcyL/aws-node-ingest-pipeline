0746e9863e8e3f21528d3c64571231be80f287e9
MalcyL <malcolm@landonsonline.me.uk>
2017-09-23 18:33:11 +0100

    Upload one at a time

--- src/ingest/lambda/ftpData.js
+++ src/ingest/lambda/ftpData.js
@@ -22,45 +22,51 @@ module.exports.ftpData = (event, context) => {
       console.error(err);
       context.fail(err);
     } else {
-      var uploaded = 0;
-      data.forEach(function (file) {
-        console.log(`Processing file ${file.name}`); 
-        var bucket = process.env.S3_BUCKET
-        var key = `${process.env.S3_KEY_PREFIX}/${file.name}`;
-        async.waterfall([
-          function(cb) {
-            console.log(`Obtaining socket for file ${file.name}`); 
-            var Ftp = new JSFtp({
-              host: process.env.FTP_HOST,
-              user: process.env.FTP_USERNAME,
-              pass: process.env.FTP_PASSWORD
-            });
-            Ftp.get(file.name,cb);
-          },
-          function upload(socket, cb) {
-            console.log(`Uploading file ${file.name} to bucket ${bucket} key: ${key}`); 
-            var awsS3 = new AWS.S3();
-            awsS3.upload({
-              Bucket: bucket,
-              Key: key,
-              Body: socket
-            }, {partSize: 10 * 1024 * 1024, queueSize: 1}, cb);
-          },
-        ],
-          function(err) {
-            if (err) {
-              console.log(`Error uploading file ${file.name} Error: ${err}`); 
-            } else {
-              console.log(`Uploaded file ${file.name}`); 
+      async.eachOfSeries(
+        data,
+        function iterate(file,key,callback) {
+          console.log(`Processing file ${file.name}`); 
+          var bucket = process.env.S3_BUCKET
+          var key = `${process.env.S3_KEY_PREFIX}/${file.name}`;
+          async.waterfall([
+            function(cb) {
+              console.log(`Obtaining socket for file ${file.name}`); 
+              var Ftp = new JSFtp({
+                host: process.env.FTP_HOST,
+                user: process.env.FTP_USERNAME,
+                pass: process.env.FTP_PASSWORD
+              });
+              Ftp.get(file.name,cb);
+            },
+            function upload(socket, cb) {
+              console.log(`Uploading file ${file.name} to bucket ${bucket} key: ${key}`); 
+              var awsS3 = new AWS.S3();
+              awsS3.upload({
+                Bucket: bucket,
+                Key: key,
+                Body: socket
+              }, {partSize: 10 * 1024 * 1024, queueSize: 1}, cb);
+            },
+          ],
+            function(err) {
+              if (err) {
+                console.log(`Error uploading file ${file.name} Error: ${err}`); 
+              } else {
+                console.log(`Uploaded file ${file.name}`); 
+              }
+              callback();
             }
-            uploaded = uploaded + 1;
-            console.log(`Uploaded ${uploaded} files out of ${data.length}`); 
-            if (uploaded == data.length) {
-              context.succeed('OK');
-            }
+          );
+        },
+        function complete(err) {
+          if (err) {
+            console.log(`Error uploading file. Error: ${err}`); 
+          } else {
+            console.log(`Uploaded files`); 
+            context.succeed('OK');
           }
-        )
-      });
+        }
+      );
     }
   });
 };
