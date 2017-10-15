71e90e8e7433ad05630242349b87e9d6a959570a
MalcyL <malcolm@landonsonline.me.uk>
2017-10-15 13:08:46 +0100

    Remove local dynamodb and check dynamodb before uploading.

--- src/ingest/lambda/ftpData.js
+++ src/ingest/lambda/ftpData.js
@@ -31,65 +31,94 @@ module.exports.ftpData = (event, context) => {
         data,
         function iterate(file,key,callback) {
           console.log(`Processing file ${file.name}`); 
-          var bucket = process.env.S3_BUCKET
-          var key = `${process.env.S3_KEY_PREFIX}/${file.name}`;
-          async.waterfall([
-            function getFtpSocket(cb) {
-              console.log(`Obtaining socket for file ${file.name}`); 
-              var Ftp = new JSFtp({
-                host: process.env.FTP_HOST,
-                user: process.env.FTP_USERNAME,
-                pass: process.env.FTP_PASSWORD
-              });
-              Ftp.get(file.name,cb);
-            },
-            function uploadToS3(socket, cb) {
-              console.log(`Uploading file ${file.name} to bucket ${bucket} key: ${key}`); 
-              var awsS3 = new AWS.S3();
-              awsS3.upload({
-                Bucket: bucket,
-                Key: key,
-                Body: socket
-              }, {partSize: 10 * 1024 * 1024, queueSize: 1}, cb);
-            },
-            function recordUploadedFilename(something,cb) {
-              console.log(`File ${file.name} uploaded. Writing filename to uploaded table...`); 
 
-              const params = {
-                TableName: process.env.DYNAMODB_TABLE_UPLOADED,
-                Item: {
-                  id: uuid.v1(),
-                  filename: file.name,
-                  checked: false,
-                  createdAt: timestamp,
-                  updatedAt: timestamp,
-                }
-              };
-            
-              // write the todo to the database
-              dynamodb.put(params, (error) => {
-                // handle potential errors
-                if (error) {
-                  console.log(`File ${file.name} uploaded BUT NOT WRITTEN TO DATABASE!`); 
-                  console.log(`Error ${error}`); 
-                  cb(error);
-                } else {
-                  console.log(`Filename ${file.name} written to database`); 
-                  cb(null);
-                }
-              });
+          var params = {
+            TableName: process.env.DYNAMODB_TABLE_UPLOADED,
+            ProjectionExpression: "filename",
+            FilterExpression: "#fn = :filename",
+            ExpressionAttributeNames: {
+              "#fn": "filename",
             },
-          ],
-            function(err) {
-              if (err) {
-                console.log(`Error uploading file ${file.name} Error: ${err}`); 
-                callback(err);
-              } else {
-                console.log(`Processing file ${file.name} completed`); 
+            ExpressionAttributeValues: {
+              ":filename": file.name
+            }
+          };
+
+          // This is a scan. Don't do this for real!
+          dynamodb.scan(params, function onScan(err, data) {
+            if (err) {
+              console.error(err);
+              callback(err);
+            } else {
+              var count = data['Count'];
+              if (count > 0) {
+                console.log(`Already uploaded file ${file.name}`); 
                 callback();
+              } else {
+                console.log(`File ${file.name} not previously uploaded`); 
+                var bucket = process.env.S3_BUCKET
+                var key = `${process.env.S3_KEY_PREFIX}/${file.name}`;
+                async.waterfall([
+                  function getFtpSocket(cb) {
+                    console.log(`Obtaining socket for file ${file.name}`); 
+                    var Ftp = new JSFtp({
+                      host: process.env.FTP_HOST,
+                      user: process.env.FTP_USERNAME,
+                      pass: process.env.FTP_PASSWORD
+                    });
+                    Ftp.get(file.name,cb);
+                  },
+                  function uploadToS3(socket, cb) {
+                    console.log(`Uploading file ${file.name} to bucket ${bucket} key: ${key}`); 
+                    var awsS3 = new AWS.S3();
+                    awsS3.upload({
+                      Bucket: bucket,
+                      Key: key,
+                      Body: socket
+                    }, {partSize: 10 * 1024 * 1024, queueSize: 1}, cb);
+                  },
+                  function recordUploadedFilename(something,cb) {
+                    console.log(`File ${file.name} uploaded. Writing filename to uploaded table...`); 
+      
+                    const params = {
+                      TableName: process.env.DYNAMODB_TABLE_UPLOADED,
+                      Item: {
+                        id: uuid.v1(),
+                        filename: file.name,
+                        checked: false,
+                        createdAt: timestamp,
+                        updatedAt: timestamp,
+                      }
+                    };
+                  
+                    // write the todo to the database
+                    dynamodb.put(params, (error) => {
+                      // handle potential errors
+                      if (error) {
+                        console.log(`File ${file.name} uploaded BUT NOT WRITTEN TO DATABASE!`); 
+                        console.log(`Error ${error}`); 
+                        cb(error);
+                      } else {
+                        console.log(`Filename ${file.name} written to database`); 
+                        cb(null);
+                      }
+                    });
+                  },
+                ],
+                  function(err) {
+                    if (err) {
+                      console.log(`Error uploading file ${file.name} Error: ${err}`); 
+                      callback(err);
+                    } else {
+                      console.log(`Processing file ${file.name} completed`); 
+                      callback();
+                    }
+                  }
+                );
               }
             }
-          );
+          });
+
         },
         function complete(err) {
           if (err) {
